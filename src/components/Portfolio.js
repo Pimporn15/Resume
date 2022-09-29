853da59756a48eae56876918fb3724479c7d1cd7
Pimporn <pimporn.kate@gmail.com>
2022-09-30 01:22:18 +0700

    Update: Update Footer

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,19 +7,46 @@ import Image4 from "../Picture/Image4.png";
 import Image5 from "../Picture/Image5.png";
 
 function Portfolio() {
-  const PicPortfolio = styled.img``;
+  const PicPortfolio = styled.img`
+    width: 310px;
+    height: 310 px;
+  `;
   const TextPortFolio = styled.div`
-    font-size: 20px;
+    width: 100vw;
+    height: 30vh;
+    font-size: 40px;
+    align-item: center;
+    background-color: #e8879f;
+    magin: 20px;
+  `;
+  const BoxPicture = styled.div`
+    width: 100vw;
+    height: 310px;
+    display: flex;
+    flex-direction: row;
+    align-item: center;
+    justify-content: center;
+    magin: 5px;
+    padding: 5px;
+  `;
+  const Footer = styled.div`
+    width: 100vw;
+    height: 15vh;
   `;
   return (
     <>
       <TextPortFolio>Portfolio</TextPortFolio>
-      <PicPortfolio src={Image0}></PicPortfolio>
-      <PicPortfolio src={Image1}></PicPortfolio>
-      <PicPortfolio src={Image2}></PicPortfolio>
-      <PicPortfolio src={Image3}></PicPortfolio>
-      <PicPortfolio src={Image4}></PicPortfolio>
-      <PicPortfolio src={Image5}></PicPortfolio>
+      <BoxPicture>
+        <PicPortfolio src={Image0}></PicPortfolio>
+        <PicPortfolio src={Image1}></PicPortfolio>
+        <PicPortfolio src={Image2}></PicPortfolio>
+      </BoxPicture>
+      <BoxPicture>
+        <PicPortfolio src={Image3}></PicPortfolio>
+        <PicPortfolio src={Image4}></PicPortfolio>
+        <PicPortfolio src={Image5}></PicPortfolio>
+      </BoxPicture>
+      <Footer></Footer>
     </>
   );
 }
